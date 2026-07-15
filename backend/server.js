dabf1ea877322fccbb4fbd6da80e0d8ea2f1d6dc
Breyner <juarezortizbreyner8@gmail.com>
2026-07-15 18:25:34 -0500

    agregar registro de peticiones

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,11 @@ app.get("/", (req, res) => {
     res.send("Servidor UrbanAlert funcionando");
 });
 
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+});
+
 app.post("/incidencias", (req, res) => {
 
     const { titulo, descripcion, categoria, ubicacion } = req.body;
